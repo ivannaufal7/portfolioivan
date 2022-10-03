61b851d0bedc75c5287a1795f7c2243c6d815f4f
ivannaufal7 <ivannaufal7@gmail.com>
2022-10-03 12:10:21 +0700

    change color btn hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -20,7 +20,7 @@ function Hero() {
         }
 
         if (!inView) {
-            animation.start({ x: '-100vw'})
+            animation.start({ x: '-100vw' })
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [inView])
@@ -33,8 +33,11 @@ function Hero() {
                 <div className='row align-items-center g-5 hero mt-1 d-flex justify-content-center'>
                     <div className='col-lg-6 text-start'>
                         <span className='display-7 font-epilogue-medium'>Ivan Naufal Islami Al Faridzqi</span><br />
-                        <span className='display-3 font-epilogue-bold'>UI/UX Designer & Front-end Dev</span><br/>
-                        <a href='https://drive.google.com/file/d/1iL8YiVVKJ2Wq22vuD5e66XMfT-JSFd8s/view?usp=sharing' target="_blank" rel="noopener noreferrer" className='btn btn-lg btn-dark'>Download Resume</a>
+                        <span className='display-3 font-epilogue-bold'>UI/UX Designer & Front-end Dev</span><br />
+                        <div className='d-flex'>
+                            <a href='https://drive.google.com/file/d/1iL8YiVVKJ2Wq22vuD5e66XMfT-JSFd8s/view?usp=sharing' target="_self" rel="noopener noreferrer" className='btn btn-lg btn-green'>Download Resume</a>
+                            <a href='https://drive.google.com/file/d/1i7t1FHHg0XKbtkcLKLPbW6UW0bT-kkHL/view' target="_self" rel="noopener noreferrer" className='btn btn-lg btn-red mx-2'>Download Portfolio</a>
+                        </div>
                     </div>
                     <div className='col-10 col-sm-8 col-lg-6 img-profiles'>
                         <Image src="../assets/images/profile.png" class="d-block mx-auto img-fluid my-2" alt="Bootstrap Themes" loading="lazy" width="80%" height="80%" />
